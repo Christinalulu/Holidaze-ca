1c1737b91bfdd9a25220eeed9b608e1f2954ec1e
Christinalulu <christinalu.fugel96@gmail.com>
2023-06-05 19:43:17 +0200

    singlelProduct ?

--- src/components/pages/Details.jsx
+++ src/components/pages/Details.jsx
@@ -1,11 +1,12 @@
-// import { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 
 const Details = () => {
-	// let {id} = useParams();
-	// const dispatch = useDispatch();
+	let {id} = useParams();
+	const dispatch = useDispatch();
+	
 	
 
 	return (
